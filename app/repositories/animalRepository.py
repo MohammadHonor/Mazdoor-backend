c05765acb353ff8f68eb1f90be730d5b76b3f89e
Mohd.Sharuf Ali <sharuf.ali@makunaiglobal.ai>
2026-02-25 23:54:15 +0530

    create a table in database

---
+++ app/repositories/animalRepository.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.schemas.animalSchema import AnimalSchema
+from app.models.animalModel import Animal 
+
+
+
+class AnimalRepo:
+    def __init__(self,session:AsyncSession):
+        self.session = session
+
+    async def create(self,animal_data:AnimalSchema)->str:
+        animal_object = Animal(
+            id=animal_data.id,
+            name=animal_data.name,
+            habitate=animal_data.habitate
+        )
+        self.session.add(animal_object)
+        await self.session.commit()
+        await self.session.refresh(animal_data)
+        return "Animal is created"
