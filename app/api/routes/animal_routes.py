5774e360e310b5f8d1f8f02190c79a906ae2e5d0
Mohd.Sharuf Ali <sharuf.ali@makunaiglobal.ai>
2026-03-05 06:41:49 +0530

    understand how to pass session

--- app/api/routes/animal_routes.py
+++ app/api/routes/animal_routes.py
@@ -1,11 +1,12 @@
 
-from fastapi import APIRouter , Depends
+from fastapi import APIRouter , Depends , status
 from app.core.endpoints import USER_BASE
 from app.repositories.animalRepository import AnimalRepo
 from app.schemas.animalSchema import AnimalSchema
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.database import get_session
 from fastapi import HTTPException
+from typing import Annotated
 
 router = APIRouter(prefix=USER_BASE,tags=["Animal api"])
 
@@ -22,8 +23,8 @@ async def add_animals(data:AnimalSchema,session:AsyncSession=Depends(get_session
 async def show_animal_list(session:AsyncSession = Depends(get_session))->dict:
     try:
         repo = AnimalRepo(session=session)
-        await repo.display()
-        return {"result":"success"}
+        return await repo.display()
+        
     except Exception as e :
         raise HTTPException(400,str(e))
 
@@ -43,4 +44,13 @@ async def show_animal_list(name:str,session:AsyncSession = Depends(get_session))
         return await repo.display_by_name(name=name)  
     except Exception as e :
         raise HTTPException(400,str(e))
+    
+@router.get("/animal/id/name")
+async def animal_search(session: Annotated[AsyncSession,Depends(get_session)])-> dict :
+    try :
+        repo = AnimalRepo(session=session)
+        return await repo.get()
+    except Exception as e :
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+
 
