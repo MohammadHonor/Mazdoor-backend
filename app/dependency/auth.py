34daea2cb4d601561daa1451373c6ea4dc5a555a
Mohd.Sharuf Ali <sharuf.ali@makunaiglobal.ai>
2026-03-28 23:38:58 +0530

    managing swagger ui error responces

---
+++ app/dependency/auth.py
@@ -0,0 +1,3 @@
+def get_current_user()->dict :
+    print("dependency")
+    return {"message": "user"}
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Depends
 from contextlib import asynccontextmanager
 from app.db.database import init_db
 from app.api.routes import router as all_routers
+from app.dependency.auth import get_current_user
+from typing import Annotated
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -10,6 +13,7 @@ async def lifespan(app: FastAPI):
     yield
     print("🛑 App is shutting down...")
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan,
+              dependencies=[Depends(get_current_user)])
 
 app.include_router(router=all_routers)
